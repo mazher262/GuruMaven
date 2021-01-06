1bd40db0422242d5b851461c9e08babc6a6d462b
mazher262 <kmazher62@gmail.com>
2021-01-06 20:19:12 +0530

    commit

--- DemoSelenium/src/main/java/com/main/pages/java/SEOClass.java
+++ DemoSelenium/src/main/java/com/main/pages/java/SEOClass.java
@@ -3,17 +3,21 @@ package com.main.pages.java;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.main.factory.java.TestBase;
 
 public class SEOClass extends TestBase {
 
+	WebDriverWait wait;
 	//@FindBy(css = "a.dropdown-toggle")
 	//@FindBy(xpath = "//a[@class='dropdown-toggle' and @data-toggle='dropdown' and @aria-expanded='false']")
 	//@FindBy(xpath="//a[@class='dropdown-toggle' and @data-toggle='dropdown' and @aria-expanded='true']")
 	//@FindBy(xpath="//li[@class='dropdown open'] ")
 	@FindBy(xpath="//*[@id=\"navbar-brand-centered\"]/ul/li[1]/ul/li[2]/a")
 	WebElement SeoMenu;
+	
 	
 	public SEOClass()
 	{
@@ -22,6 +26,9 @@ public class SEOClass extends TestBase {
 	
 	public void seoMenuClick()
 	{
+		wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.visibilityOf(SeoMenu)); 
+		wait.until(ExpectedConditions.elementToBeClickable(SeoMenu));
 		SeoMenu.click();
 	}
 }
