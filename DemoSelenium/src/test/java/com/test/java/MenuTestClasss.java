76d98c1b2f0a6f2305a03c64ae670c5af4f3eca4
mazher262 <kmazher62@gmail.com>
2021-01-10 19:17:28 +0530

    c

--- DemoSelenium/src/test/java/com/test/java/MenuTestClasss.java
+++ DemoSelenium/src/test/java/com/test/java/MenuTestClasss.java
@@ -4,14 +4,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class MenuTestClasss {
  
 	ChromeDriver driver;
-	
+	@BeforeMethod (alwaysRun = true)
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\htmlunit-driver-2.45.0\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.guru99.com/");
+		//WebElement menu = driver.findElement(By.xpath(xpathExpression));
+	}
 	@Test
-  public void f() {
+  public void MenuTestDemo() {
 	  Actions actions = new Actions(driver);
 		WebElement mainMenu = driver.findElement(By.xpath("//span[contains(text()='Testing')"));
 		//WebElement mainMenu = driver.findElement(By.cssSelector(//span[contains(text()='Testing')"));
