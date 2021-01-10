9ac5ed37af7f9397266509fc0196c884780213fa
mazher262 <kmazher62@gmail.com>
2021-01-10 19:17:13 +0530

    c

---
+++ DemoSelenium/src/test/java/com/test/java/MenuTestClasss.java
@@ -0,0 +1,24 @@
+package com.test.java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class MenuTestClasss {
+ 
+	ChromeDriver driver;
+	
+	@Test
+  public void f() {
+	  Actions actions = new Actions(driver);
+		WebElement mainMenu = driver.findElement(By.xpath("//span[contains(text()='Testing')"));
+		//WebElement mainMenu = driver.findElement(By.cssSelector(//span[contains(text()='Testing')"));
+		actions.moveToElement(mainMenu);
+
+		WebElement subMenu = driver.findElement(By.xpath("//span[contains(text()='LoadRunner')"));
+		actions.moveToElement(subMenu);
+		actions.click().build().perform();
+  }
+}
