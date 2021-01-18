8667116350fdf5fcb3996c2f2c8ba21225618fb3
mazher262 <kmazher62@gmail.com>
2021-01-18 16:27:11 +0530

    c

--- DemoSelenium/src/test/java/com/test/java/TableDemoTestClass.java
+++ DemoSelenium/src/test/java/com/test/java/TableDemoTestClass.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import com.main.factory.java.TestBase;
 import com.main.pages.java.GuruHomePage;
 import com.main.pages.java.LoginPage;
+import com.main.pages.java.TableDemoClass;
 
 public class TableDemoTestClass extends TestBase{
   
@@ -26,6 +27,7 @@ public class TableDemoTestClass extends TestBase{
 	
 	@Test
   public void TableDemoClick() {
-		
+		TableDemoClass tdc = new TableDemoClass();
+		tdc.clickTableDemo();
   }
 }
