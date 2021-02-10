cef1b361a6954ec2b30fecdec3f24973b4559fba
mazher262 <kmazher62@gmail.com>
2021-02-10 21:11:00 +0530

    c

--- DemoSelenium/src/main/java/com/main/factory/java/TestBase.java
+++ DemoSelenium/src/main/java/com/main/factory/java/TestBase.java
@@ -73,6 +73,7 @@ public class TestBase {
 		
 		//driver.manage().window().window()).maximize();
 		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
 		driver.get(prop.getProperty("url"));
 		
